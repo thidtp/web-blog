c62196b5b684bbc1b48299d97c576123d8d8811e
nguyenthikimdung-02 <kimdungche@gmail.com>
2025-04-21 22:33:29 +0700

    reponsive
    reponsive final

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-scroll";
 import Logo from "../../assets/images/logo.gif";
 import "./Header.css";
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
     return(
         <section className="header" id="header">
             <Link to="home" className="logo-header">
                 <img src={Logo} alt="Logo-Header" />
             </Link>
-            <nav className="nav_header">
+
+            <div className="menu-toggle" onClick={toggleMenu}>
+                ☰
+            </div>
+
+            <nav className={`nav_header ${isOpen ? "active" : ""}`}>
                 <Link to="home" smooth = {true} duration = {500} offset={-100} className="home active">Trang Chủ</Link>
-                <Link to="about" smooth = {true} duration = {500} offset={-70} className="about">Giới Thiệu</Link>
+                <Link to="about" smooth = {true} duration = {500} offset={-100} className="about">Giới Thiệu</Link>
                 <Link to="project" smooth = {true} duration = {500} offset={-100}  className="">Dự Án</Link>
                 <Link to="contact" smooth = {true} duration = {500} offset={-100}  className="nav_link_header">Liên Hệ</Link>
             </nav>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
     return(
         <section className="hero" id="home">
             <div className="hero-content">
-                <h1 className="hero-title">Đồng hành cùng sinh viên chinh <br /> phục đồ án</h1>
+                <h1 className="hero-title">Đồng hành cùng sinh viên chinh phục đồ án</h1>
                 <p className="hero-text">Nhận hỗ trợ thực hiện đồ án theo yêu cầu |  Nhiều <br />  nền tảng | Đảm bảo đúng tiến độ</p>
                 <Button1 />
                 {/* <Button LinkComponent={Link} to="contact" smooth = {true} duration = {500} offset={-100} sx={{ boxShadow: 'none','&:focus': {outline: 'none',},'&:focus-visible': {outline: 'none',boxShadow: 'none',},}} variant="contained" className="hero-btn">Liên Hệ Ngay</Button> */}
