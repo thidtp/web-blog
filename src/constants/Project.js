9b482e30a8024d75702b40288147c569fd01bcd3
nguyenthikimdung-02 <kimdungche@gmail.com>
2025-04-21 23:19:50 +0700

    update project part1

--- src/constants/Project.js
+++ src/constants/Project.js
@@ -1,10 +1,24 @@
 import projectImage1 from "../assets/images/project-image1.png";
+import projectImage2 from "../assets/images/project-image2.png";
+import projectImage3 from "../assets/images/project-image3.png";
 export const projectContent = [
     {
         title: "Dự án quản lý giao hàng online",
         description: "Phần mềm quản lý kho hàng cho doanh nghiệp vừa và nhỏ, hỗ trợ theo dõi đơn hàng, quản lý kho, vận chuyển và xử lý sự cố. Đây là đồ án chuyên ngành của sinh viên năm cuối và đã đạt điểm A.",
         image: projectImage1,
-        technologies: ["React", "Node.js", "MongoDB"],
+        technologies: ["Java", "String Boot", "Neo4j"],
+    },
+    {
+        title: "Dự án quản lý hoạt động sinh viên nghiên cứu khoa học tại khoa công nghệ thông tin",
+        description: "Quản lý, theo dõi và đảm bảo rằng hoạt động nghiên cứu khoa học của sinh viên tại Khoa Công nghệ Thông tin được diễn ra  hiệu quả và thuận tiện.Đây là khóa luận tốt nghiệp của sinh viên năm cuối theo học tại trường đã đạt điểm A. Ứng dụng thực thi trên 2 nền tảng: website và app mobile với các chức năng theo phân quyền hệ thống. ",
+        image: projectImage3,
+        technologies: ["C#","Java", "Android Studio", "SQL Sever", "Web API", "ReactJS"],
+    },
+    {
+        title: "Dự án quản lý cửa hàng quần áo",
+        description: "Phát triển một ứng dụng di động nhằm hỗ trợ các cửa hàng quần áo trong việc quản lý và vận hành kinh doanh hiệu quả. Dự án này được điểm A.",
+        image: projectImage2,
+        technologies: ["Java", "Android Studio", "SQL Sever", "Web API"],
     },
     {
         title: "Ứng Dụng Quản Lý Công Việc",
